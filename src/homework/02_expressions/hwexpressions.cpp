d5c333b3c3cca92486ba5a89e07bce8bd828cb24
Davis <123427626+daviscpp@users.noreply.github.com>
2023-02-12 01:16:44 +0000

    hw-02-expressions

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,5 +1,5 @@
 #include "expressions.h"
-
+#include "hwexpressions.h"
 //example
 int add_numbers(int num1, int num2)
 {
@@ -7,7 +7,13 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+double get_sales_tax_amount(double meal_amount)
+{
+	const double tax_rate = 0.0675;
+	return meal_amount * tax_rate;
+}
 
-
-
-
+double get_tip_amount(double meal_ammount, double tip_rate)
+{
+	return meal_ammount * tip_rate;
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,31 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
+	double meal_amount, tip_rate, tip_amount, tax_amount, total = 0;
+
+	cout << "Enter meal amount: ";
+	cin >> meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout << "Enter tip rate: ";
+	cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
 	
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout << total;
 
 	return 0;
 }
