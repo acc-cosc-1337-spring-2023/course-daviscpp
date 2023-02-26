c0a60e329621da1825c2a0259383d44cc92728c9
davis <123427626+daviscpp@users.noreply.github.com>
2023-02-26 01:21:19 +0000

    hw-04

--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,5 +1,12 @@
 //add include statements
+#include <iostream>
 
+using std::cout; using std::cin; using std::endl;
 /*
 Write prototype for function(s)
 */
+int factorial(int num);
+int gcd(int num1, int num2);
+void menu();
+void run_menu();
+void handle_menu_option(int num);
